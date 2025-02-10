3f0b0f2b2444b9008b33755f6b1d241ac05e4f99
supu345 <supu345@gmail.com>
2025-02-10 21:17:04 +0600

    url work

--- server.js
+++ server.js
@@ -5,50 +5,46 @@ const body_parser = require("body-parser");
 const cors = require("cors");
 const db_connect = require("./utils/db");
 const paginate = require("express-paginate");
+
 dotenv.config();
 
-app.use(body_parser.json());
-app.use(paginate.middleware(10, 50));
-
-// if (process.env.mode === "production") {
-//   app.use(cors());
-// } else {
-//   app.use(
-//     cors({
-//       origin: [
-//         "http://localhost:5173",
-//         "http://localhost:5174",
-//         "http://localhost:3000",
-//       ],
-//     })
-//   );
-// }
+const allowedOrigins =
+  process.env.mode === "pro"
+    ? [
+        process.env.CLIENT_CUSTOMER_PRODUCTION_URL,
+        process.env.CLIENT_CUSTOMER_SECONDARY_URL,
+      ]
+    : [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:3000",
+      ];
 
 app.use(
   cors({
-    origin:
-      process.env.mode === "pro"
-        ? [
-            process.env.CLIENT_CUSTOMER_PRODUCTION_URL,
-            process.env.CLIENT_CUSTOMER_SECONDARY_URL,
-          ]
-        : [
-            "http://localhost:5173",
-            "http://localhost:5174",
-            "https://blog-app03-client.vercel.app",
-            "https://daily-news-client.vercel.app",
-          ],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // Allow requests with no origin
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS policy: Origin not allowed"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
+app.use(body_parser.json());
+app.use(paginate.middleware(10, 50));
 app.use("/", require("./routes/authRoutes"));
-app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/", require("./routes/newsRoute"));
-const port = process.env.port;
+
+const port = process.env.port || 5000;
 
 db_connect();
 
-app.listen(port, () => console.log(`server is running on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Server is running on port ${port} in mode ${process.env.mode}`)
+);
